02be738a32866d57c5c2a3ef114ac8b3c10bde66
Jordan Borean <jborean93@gmail.com>
2020-10-10 05:11:46 +1000

    Added GSSAPI auth endpoint (#8)

--- services.sh
+++ services.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+if [ -z ${KRB5_PASSWORD} ]; then
+    echo "No KRB5_PASSWORD provided for the admin account."
+    exit 1
+fi
+
+kadmin.local -q "addprinc -pw ${KRB5_PASSWORD} admin"
+/usr/sbin/krb5kdc
 gunicorn -D httpbin:app
 nginx -g "daemon off;"
